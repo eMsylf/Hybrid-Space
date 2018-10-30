74388ea67c448bedcc0fe8a2b31a4d5bee461bf3
Felix Buenen <felixbuenen@hotmail.com>
2018-10-30 16:08:10 +0100

    Playing around with 3D object placement

--- HybridSpace-Team15-UnityProj/Assets/Scripts/PlayerMovement.cs
+++ HybridSpace-Team15-UnityProj/Assets/Scripts/PlayerMovement.cs
@@ -1,22 +1,46 @@
 ﻿using UnityEngine;
+using Vuforia;
 
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerMovement : MonoBehaviour {
 
 	public float speed;
+	public bool active;
+	public GameObject platformPrefab;
 
 	Rigidbody rb;
 
 	// Use this for initialization
 	void Start () {
-
+		active = false;
 		rb = GetComponent<Rigidbody>();
-	
 	}
 	
 	// only move player when colliding with platform
 	void OnCollisionStay(Collision c)
 	{
-		rb.AddForce(Vector3.left * speed);
+		if (active)
+		{
+			rb.AddForce(Vector3.left * speed);
+			Debug.Log("Should move");
+		}
 	}
+
+	public void EnableMovement()
+	{
+		GameObject[] platforms = GameObject.FindGameObjectsWithTag("Platform");
+		foreach(GameObject platform in platforms)
+		{
+			Vector3 pos = platform.transform.position;
+			pos.z = 20;
+			Instantiate(platformPrefab, pos, Quaternion.identity);
+			Destroy(platform);
+		}
+		
+		
+		TrackerManager.Instance.GetTracker<ObjectTracker>().Stop();
+		//active = true;
+		//rb.AddForce(Vector3.left * speed);
+	}
+	
 }
---
+++ HybridSpace-Team15-UnityProj/Assets/Scripts/TrackManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using Vuforia;
+
+public class TrackManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void StopTracking()
+	{
+		
+	}
+}
