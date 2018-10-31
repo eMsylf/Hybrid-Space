290cb30dbd428470f22054c2d000ec44f7d8894c
Felix Buenen <felixbuenen@hotmail.com>
2018-10-31 13:37:22 +0100

    3d positioning / resizing of platform works

---
+++ HybridSpace-Team15-UnityProj/Assets/Scripts/Resize.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Resize : MonoBehaviour {
+
+	public Camera cam;
+	public float newZ;
+
+	public void DoResize()
+	{
+		float oldDistance = (gameObject.transform.position - cam.transform.position).magnitude;
+		Vector3 newBoxPos = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, newZ);
+
+		float newDistance = (newBoxPos - cam.transform.position).magnitude;
+		float newSize = (newDistance/oldDistance);
+
+		// REPOSITIONING
+		Vector3 screenPos = cam.WorldToScreenPoint(transform.position);
+		Vector3 worldPos = cam.ScreenToWorldPoint(new Vector3(screenPos.x, screenPos.y, newZ));
+		
+		transform.position = worldPos;
+		transform.localScale = Vector3.one * newSize;
+
+	}
+
+}
