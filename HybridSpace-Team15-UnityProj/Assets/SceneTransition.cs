db968f3ea8389cb6adb88131cdd4d66d2d457fd6
Felix Buenen <felixbuenen@hotmail.com>
2018-12-11 10:35:09 +0100

    scene transitions

---
+++ HybridSpace-Team15-UnityProj/Assets/SceneTransition.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneTransition : MonoBehaviour
+{
+  public static SceneTransition Instance;
+
+  // Use this for initialization
+  void Awake()
+  {
+    if (Instance == null)
+    {
+      Instance = this;
+    }
+    else if (Instance != this)
+    {
+      Destroy(gameObject);
+    }
+
+    DontDestroyOnLoad(gameObject);
+  }
+
+  public void GoToNextLevel()
+  {
+    int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+    if (SceneManager.sceneCountInBuildSettings > nextSceneIndex)
+    {
+      SceneManager.LoadScene(nextSceneIndex);
+    }
+    else
+    {
+      GameObject finishText = GameObject.Find("FinishText");
+      finishText.GetComponent<Text>().enabled = true;
+    }
+  }
+}
--- HybridSpace-Team15-UnityProj/Assets/Scripts/Triggers/FinishTrigger.cs
+++ HybridSpace-Team15-UnityProj/Assets/Scripts/Triggers/FinishTrigger.cs
@@ -3,13 +3,11 @@ using UnityEngine.UI;
 
 public class FinishTrigger : MonoBehaviour
 {
-
-  public GameObject finishText;
-
   void OnTriggerEnter(Collider collider)
   {
-    finishText.SetActive(true);
     collider.transform.GetComponent<PlayerMovementSimulated>().EnableMovement(false);
     collider.transform.GetComponent<Rigidbody>().velocity = Vector3.zero;
+
+    SceneTransition.Instance.GoToNextLevel();
   }
 }
