4e1e87d942797e84c09fd01017f6cdaed2814df4
Felix Buenen <felixbuenen@hotmail.com>
2018-12-11 11:43:31 +0100

    add music player

---
+++ HybridSpace-Team15-UnityProj/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum MusicType
+{
+  AMBIENT,
+  MELODIC,
+  NONE
+}
+
+public class MusicPlayer : MonoBehaviour
+{
+  public AudioClip ambientClip;
+  public AudioClip melodicClip;
+  public MusicType musicType;
+
+  private AudioSource audioSource;
+
+  private MusicPlayer Instance;
+
+  // Use this for initialization
+  void Start()
+  {
+    if (Instance == null)
+    {
+      Instance = this;
+    }
+    // only one music player in scene
+    else if (Instance != this)
+    {
+      Destroy(gameObject);
+    }
+
+    audioSource = GetComponent<AudioSource>();
+
+    switch (musicType)
+    {
+      case MusicType.AMBIENT:
+        audioSource.clip = ambientClip;
+        audioSource.Play();
+        break;
+      case MusicType.MELODIC:
+        audioSource.clip = melodicClip;
+        audioSource.Play();
+        break;
+      default:
+        break;
+    }
+
+    DontDestroyOnLoad(gameObject);
+  }
+
+}
