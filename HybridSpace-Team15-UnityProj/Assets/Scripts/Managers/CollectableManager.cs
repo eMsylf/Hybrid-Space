bc3a2039a881dd9596a4638947ee4f6afca015fe
Felix Buenen <felixbuenen@hotmail.com>
2018-12-11 12:01:37 +0100

    add collectables

---
+++ HybridSpace-Team15-UnityProj/Assets/Scripts/Managers/CollectableManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectableManager : MonoBehaviour
+{
+
+  public static CollectableManager Instance;
+
+  // container for total score in game
+  public int Score
+  {
+    get;
+    set;
+  }
+
+  // Use this for initialization
+  void Awake()
+  {
+    if (Instance == null)
+    {
+      Instance = this;
+    }
+    else if (Instance != this)
+    {
+      Destroy(gameObject);
+    }
+
+    DontDestroyOnLoad(gameObject);
+  }
+}
---
+++ HybridSpace-Team15-UnityProj/Assets/Scripts/Triggers/CollectableTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectableTrigger : MonoBehaviour
+{
+  void OnTriggerEnter(Collider c)
+  {
+    if (c.gameObject.tag == "Player")
+    {
+      CollectableManager.Instance.Score++;
+      Destroy(gameObject); // get rid of collectable
+
+      Debug.Log(CollectableManager.Instance.Score);
+    }
+  }
+
+}
