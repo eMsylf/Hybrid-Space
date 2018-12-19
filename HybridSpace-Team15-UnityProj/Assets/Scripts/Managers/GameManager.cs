4414b0f44f344091cc0e8c5e5eff286f1e901750
Bob <bobjeltes@hotmail.com>
2018-12-19 14:13:04 +0100

    Changes to prefabs, Vuforia config
    - Changed the tags of all grass prefabs to Blockade so that the player can't place platforms inside them
    - Reverted the change in Vuforia config from CAMERA_FRONT to CAMERA_DEFAULT because the project doesn't work in Play Mode if CAMERA_FRONT is selected
    
    - Need to add something to a script that prevents the player from placing a blockade at the boar's position

--- HybridSpace-Team15-UnityProj/Assets/Scripts/Managers/GameManager.cs
+++ HybridSpace-Team15-UnityProj/Assets/Scripts/Managers/GameManager.cs
@@ -5,162 +5,143 @@ using UnityEngine.UI;
 using Vuforia;
 
 [System.Serializable]
-public struct Checkpoint
-{
-  public Vector3 playerPosition;
-  public Vector3 cameraPosition;
+public struct Checkpoint {
+    public Vector3 playerPosition;
+    public Vector3 cameraPosition;
 }
 
-public class GameManager : MonoBehaviour
-{
-  public static GameManager instance = null;
-  public GameObject normalPlatform;
-  public GameObject jumpPlatform;
-  public Transform[] checkpointTransforms;
+public class GameManager : MonoBehaviour {
+    public static GameManager instance = null;
+    public GameObject normalPlatform;
+    public GameObject jumpPlatform;
+    public Transform[] checkpointTransforms;
 
-  private GameObject player;
-  private int activeCheckpointIndex;
+    public GameObject player;
+    private int activeCheckpointIndex;
 
-  public int ActiveCheckpointIndex { get { return activeCheckpointIndex; } }
-  public Transform ActiveCheckpoint { get { return checkpointTransforms[activeCheckpointIndex]; } }
+    public int ActiveCheckpointIndex { get { return activeCheckpointIndex; } }
+    public Transform ActiveCheckpoint { get { return checkpointTransforms[activeCheckpointIndex]; } }
 
-  // Use this for initialization
-  void Start()
-  {
-    if (instance == null)
-      instance = this;
+    // Use this for initialization
+    void Start() {
+        if (instance == null)
+            instance = this;
 
-    else if (instance != this)
-      Destroy(gameObject);
+        else if (instance != this)
+            Destroy(gameObject);
 
 
-    player = GameObject.FindGameObjectWithTag("Player");
+        player = GameObject.FindGameObjectWithTag("Player");
 
-  }
-
-  private void Update()
-  {
-    if (Input.GetKeyDown(KeyCode.Space))
-    {
-      StartSimulation();
-    }
-  }
-
-  // called when player presses "simulate"
-  public void StartSimulation()
-  {
-    SpawnPlatforms();
-
-    bool blockadeCollision = CollidingWithBlockade();
-    Debug.Log("Blockade collision: " + blockadeCollision);
-
-    if (!blockadeCollision)
-    {
-      player.GetComponent<PlayerMovementSimulated>().EnableMovement(true);
-      TrackerManager.Instance.GetTracker<ObjectTracker>().Stop();
-    }
-    else
-    {
-      ResetLevel();
-    }
-  }
-
-  // called when player dies
-  public void ResetSimulation()
-  {
-    player.GetComponent<PlayerMovementSimulated>().EnableMovement(false);
-    player.transform.position = ActiveCheckpoint.position;
-
-    ResetLevel();
-  }
-
-  public void NextCheckpoint()
-  {
-    player.GetComponent<PlayerMovementSimulated>().EnableMovement(false);
-    activeCheckpointIndex++;
-    Camera.main.GetComponent<NextCheckpointPosition>().GoToNextCheckPoint(checkpointTransforms[activeCheckpointIndex]);
-
-    ResetLevel();
-  }
-
-  private void ResetLevel()
-  {
-    // remove platforms
-    GameObject[] platforms = GameObject.FindGameObjectsWithTag("Platform");
-    foreach (GameObject platform in platforms)
-    {
-      // only get rendered platforms
-      Destroy(platform);
     }
 
-    // start tracking
-    TrackerManager.Instance.GetTracker<ObjectTracker>().Start();
-  }
-
-  private void SpawnPlatforms()
-  {
-    GameObject[] platforms = GameObject.FindGameObjectsWithTag("Target");
-    foreach (GameObject platform in platforms)
-    {
-      // only get rendered platforms
-      if (!platform.transform.GetChild(0).GetComponent<SpriteRenderer>().enabled) continue;
-
-      Vector3 pos = platform.transform.position;
-      Quaternion rot = platform.transform.rotation;
-      string type = platform.transform.GetChild(0).name;
-      GameObject platformPrefab = null;
-      switch (type)
-      {
-        case "NormalPlatform":
-          platformPrefab = normalPlatform;
-          break;
-        case "JumpPlatform":
-          platformPrefab = jumpPlatform;
-          break;
-        default:
-          Debug.Log("ERROR: platform not valid");
-          return;
-      }
-
-      GameObject platform_instance = (GameObject)Instantiate(platformPrefab, pos, rot);
-      SetScaleAndPosition(ref platform_instance);
-
-      platform.transform.GetChild(0).GetComponent<SpriteRenderer>().enabled = false;
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            StartSimulation();
+        }
     }
-  }
 
-  private bool CollidingWithBlockade()
-  {
-    GameObject[] blockades = GameObject.FindGameObjectsWithTag("Blockade");
-    GameObject[] platforms = GameObject.FindGameObjectsWithTag("Platform");
-    foreach (GameObject blockade in blockades)
-    {
-      //Debug.Log("There is a blockade, its name is: " + blockade.name);
-      //Debug.Log("Its collision value is: " + blockade.GetComponent<BlockadeCollision>().IsColliding());
-      if (blockade.GetComponent<BlockadeCollision>().IsColliding(platforms))
-      {
-        return true;
-      }
+    // called when player presses "simulate"
+    public void StartSimulation() {
+        SpawnPlatforms();
+
+        bool blockadeCollision = CollidingWithBlockade();
+        Debug.Log("Blockade collision: " + blockadeCollision);
+
+        if (!blockadeCollision) {
+            player.GetComponent<PlayerMovementSimulated>().EnableMovement(true);
+            TrackerManager.Instance.GetTracker<ObjectTracker>().Stop();
+        } else {
+            ResetLevel();
+        }
     }
-    return false;
-  }
 
-  private void SetScaleAndPosition(ref GameObject obj)
-  {
-    Camera cam = Camera.main;
+    // called when player dies
+    public void ResetSimulation() {
+        player.GetComponent<PlayerMovementSimulated>().EnableMovement(false);
+        player.transform.position = ActiveCheckpoint.position;
 
-    float oldDistance = (obj.transform.position - cam.transform.position).magnitude;
-    Vector3 newBoxPos = new Vector3(obj.transform.position.x, obj.transform.position.y, 19);
+        ResetLevel();
+    }
 
-    float newDistance = (newBoxPos - cam.transform.position).magnitude;
-    float newSize = (newDistance / oldDistance);
+    public void NextCheckpoint() {
+        player.GetComponent<PlayerMovementSimulated>().EnableMovement(false);
+        activeCheckpointIndex++;
+        Camera.main.GetComponent<NextCheckpointPosition>().GoToNextCheckPoint(checkpointTransforms[activeCheckpointIndex]);
 
-    // REPOSITIONING
-    Vector3 screenPos = cam.WorldToScreenPoint(obj.transform.position);
-    Vector3 worldPos = cam.ScreenToWorldPoint(new Vector3(screenPos.x, screenPos.y, 19));
+        ResetLevel();
+    }
 
-    obj.transform.position = worldPos;
-    obj.transform.localScale = Vector3.one * newSize * obj.transform.localScale.x;
-    obj.transform.rotation = Quaternion.identity;
-  }
+    private void ResetLevel() {
+        // remove platforms
+        GameObject[] platforms = GameObject.FindGameObjectsWithTag("Platform");
+        foreach (GameObject platform in platforms) {
+            // only get rendered platforms
+            Destroy(platform);
+        }
+
+        // start tracking
+        TrackerManager.Instance.GetTracker<ObjectTracker>().Start();
+    }
+
+    private void SpawnPlatforms() {
+        GameObject[] platforms = GameObject.FindGameObjectsWithTag("Target");
+        foreach (GameObject platform in platforms) {
+            // only get rendered platforms
+            if (!platform.transform.GetChild(0).GetComponent<SpriteRenderer>().enabled) continue;
+
+            Vector3 pos = platform.transform.position;
+            Quaternion rot = platform.transform.rotation;
+            string type = platform.transform.GetChild(0).name;
+            GameObject platformPrefab = null;
+            switch (type) {
+                case "NormalPlatform":
+                    platformPrefab = normalPlatform;
+                    break;
+                case "JumpPlatform":
+                    platformPrefab = jumpPlatform;
+                    break;
+                default:
+                    Debug.Log("ERROR: platform not valid");
+                    return;
+            }
+
+            GameObject platform_instance = (GameObject)Instantiate(platformPrefab, pos, rot);
+            SetScaleAndPosition(ref platform_instance);
+
+            platform.transform.GetChild(0).GetComponent<SpriteRenderer>().enabled = false;
+        }
+    }
+
+    private bool CollidingWithBlockade() {
+        GameObject[] blockades = GameObject.FindGameObjectsWithTag("Blockade");
+        GameObject[] platforms = GameObject.FindGameObjectsWithTag("Platform");
+
+        foreach (GameObject blockade in blockades) {
+            //Debug.Log("There is a blockade, its name is: " + blockade.name);
+            //Debug.Log("Its collision value is: " + blockade.GetComponent<BlockadeCollision>().IsColliding());
+            if (blockade.GetComponent<BlockadeCollision>().IsColliding(platforms)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void SetScaleAndPosition(ref GameObject obj) {
+        Camera cam = Camera.main;
+
+        float oldDistance = (obj.transform.position - cam.transform.position).magnitude;
+        Vector3 newBoxPos = new Vector3(obj.transform.position.x, obj.transform.position.y, 19);
+
+        float newDistance = (newBoxPos - cam.transform.position).magnitude;
+        float newSize = (newDistance / oldDistance);
+
+        // REPOSITIONING
+        Vector3 screenPos = cam.WorldToScreenPoint(obj.transform.position);
+        Vector3 worldPos = cam.ScreenToWorldPoint(new Vector3(screenPos.x, screenPos.y, 19));
+
+        obj.transform.position = worldPos;
+        obj.transform.localScale = Vector3.one * newSize * obj.transform.localScale.x;
+        obj.transform.rotation = Quaternion.identity;
+    }
 }
