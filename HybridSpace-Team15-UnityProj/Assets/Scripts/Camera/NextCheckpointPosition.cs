66064469155e7f7e7d0a1b8761c2e4628776dcb2
Felix Buenen <felixbuenen@hotmail.com>
2019-01-13 15:25:58 +0100

    fix camera position issue

--- HybridSpace-Team15-UnityProj/Assets/Scripts/Camera/NextCheckpointPosition.cs
+++ HybridSpace-Team15-UnityProj/Assets/Scripts/Camera/NextCheckpointPosition.cs
@@ -2,20 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NextCheckpointPosition : MonoBehaviour {
+public class NextCheckpointPosition : MonoBehaviour
+{
     public float offset;
-
     public Transform MidPointTransform;
 
-    private void Start() {
-        MidPointTransform = FindObjectOfType<MidPointScript>().transform;
-        offset = -MidPointTransform.position.x;
-        Debug.Log("Set new camera midpoint offset");
+    private void Start()
+    {
+        //MidPointTransform = FindObjectOfType<MidPointScript>().transform;
+        //offset = -MidPointTransform.position.x;
+        //        Debug.Log("Set new camera midpoint offset");
     }
 
-    public void GoToNextCheckPoint(Transform activeCheckpoint) {
-
-
+    public void GoToNextCheckPoint(Transform activeCheckpoint)
+    {
         Vector3 camEdgeWorldPoint = Camera.main.ViewportToWorldPoint(new Vector3(0, activeCheckpoint.position.y, 19f));
         float length = (activeCheckpoint.position - camEdgeWorldPoint).magnitude;
 
