d061ed3c6f8861cab941c7db532ab6c1c6f9fdb9
Bob <bobjeltes@hotmail.com>
2019-01-13 14:41:24 +0100

    Level setup for level 4 and 5 applied, dynamic camera position fix start
    Level setup
    - Applied levels 4 and 5 setup as done in level 1, 2 and 3
    - Made collectables prefab (marginal addition but still nice to have)
    - Placed apples in levels 4 and 5
    
    Camera
    - Tried my hand at a dynamic camera position that looks at the midpoint's x position to determine the position it needs to assume after the player has reached the midpoint.
    - These were added to the prefab, so it should be automated for all levels.
    - Works in the basics but needs to be refined to be functional.

--- HybridSpace-Team15-UnityProj/Assets/Scripts/Camera/NextCheckpointPosition.cs
+++ HybridSpace-Team15-UnityProj/Assets/Scripts/Camera/NextCheckpointPosition.cs
@@ -2,18 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NextCheckpointPosition : MonoBehaviour
-{
-  public float offset;
+public class NextCheckpointPosition : MonoBehaviour {
+    public float offset;
 
-  public void GoToNextCheckPoint(Transform activeCheckpoint)
-  {
-    Vector3 camEdgeWorldPoint = Camera.main.ViewportToWorldPoint(new Vector3(0, activeCheckpoint.position.y, 19f));
-    float length = (activeCheckpoint.position - camEdgeWorldPoint).magnitude;
+    public Transform MidPointTransform;
 
-    Vector3 newPosition = Camera.main.transform.position;
-    newPosition.x -= length - offset;
+    private void Start() {
+        MidPointTransform = FindObjectOfType<MidPointScript>().transform;
+        offset = -MidPointTransform.position.x;
+        Debug.Log("Set new camera midpoint offset");
+    }
 
-    transform.position = newPosition;
-  }
+    public void GoToNextCheckPoint(Transform activeCheckpoint) {
+
+
+        Vector3 camEdgeWorldPoint = Camera.main.ViewportToWorldPoint(new Vector3(0, activeCheckpoint.position.y, 19f));
+        float length = (activeCheckpoint.position - camEdgeWorldPoint).magnitude;
+
+        Vector3 newPosition = Camera.main.transform.position;
+        newPosition.x -= length - offset;
+
+        transform.position = newPosition;
+    }
 }
