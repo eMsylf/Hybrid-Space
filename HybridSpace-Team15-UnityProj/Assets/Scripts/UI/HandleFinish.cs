c78dff99e24f71d10da1fbf03f8a3a6a69fe52c4
Felix Buenen <felixbuenen@hotmail.com>
2019-01-15 17:41:44 +0100

    add button functionality of finish panel

---
+++ HybridSpace-Team15-UnityProj/Assets/Scripts/UI/HandleFinish.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HandleFinish : MonoBehaviour
+{
+	public void GoToMainMenu()
+	{
+		CleanUp();
+		SceneManager.LoadSceneAsync("Scenes/Menu");
+	}
+
+	public void RestartGame()
+	{
+		CleanUp();
+		SceneManager.LoadSceneAsync("Scenes/Lvl01");
+	}
+
+	private void CleanUp()
+	{
+		GameObject sceneTransition = GameObject.Find("SceneTransition");
+		GameObject musicPlayer = GameObject.Find("MusicPlayer");
+		GameObject collectableManager = GameObject.Find("CollectableManager");
+		GameObject checkpointScore = GameObject.Find("_CheckpointScore");
+
+		Destroy(sceneTransition);
+		Destroy(musicPlayer);
+		Destroy(collectableManager);
+		Destroy(checkpointScore);
+	}
+
+}
